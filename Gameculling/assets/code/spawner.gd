86206089b9af35db264197ecd431493c068f5310
MarcinMirocha <60358010+Marcin-sys@users.noreply.github.com>
2020-03-03 02:03:38 +0100

    Changed: objects now generate on the area from 1 to 10 along the x and z axis. Changed position of Camera.

--- Gameculling/assets/code/spawner.gd
+++ Gameculling/assets/code/spawner.gd
@@ -3,7 +3,7 @@ extends Node
 var cube_scene = preload("res://scenes/cube.tscn")
 var cylinder_scene = preload("res://scenes/cylinder.tscn")
 var sphere_scene = preload("res://scenes/sphere.tscn")
-var y = 0
+var count = 0
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -12,23 +12,23 @@ var y = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#var cube_node = cube_scene.instance()
-	#self.add_child(cube_node)
+	var cube_node = cube_scene.instance()
+	self.add_child(cube_node)
 	var _delta = 1
-
-
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-
-		if  y < 10:
+	
+		if  count < 10:
 			randomize()
+			
 			var list = [ cube_scene.instance() , cylinder_scene.instance() , sphere_scene.instance() ]	
 			var drawned = list[randi() % list.size()]
+			var random_position = Vector3(randi() % 11 + 1 , 0, randi() % 11 + 1)
+			drawned.set_translation(random_position)
 			self.add_child(drawned)
-			y+=1	
+			count+=1
 		else:
 			pass
-
