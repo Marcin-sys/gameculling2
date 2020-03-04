09d2340708e0f41dfc3b81b37ba55f70cc27ff4b
MarcinMirocha <60358010+Marcin-sys@users.noreply.github.com>
2020-03-04 01:54:16 +0100

    Added:  After 1 second generate 1 object till u have 10.   Func _ready commented. Fix Typo.  Changed Vector3 from rand() to randi_range.

--- Gameculling/assets/code/spawner.gd
+++ Gameculling/assets/code/spawner.gd
@@ -3,32 +3,36 @@ extends Node
 var cube_scene = preload("res://scenes/cube.tscn")
 var cylinder_scene = preload("res://scenes/cylinder.tscn")
 var sphere_scene = preload("res://scenes/sphere.tscn")
-var count = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var Node_counter = 0	# For counting the number of objects on the screen
+const TIME_SPAN = 1     # Time period till function spawn another object
+var time_left = 0		# Timer
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var cube_node = cube_scene.instance()
-	self.add_child(cube_node)
-	var _delta = 1
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+# Called when the node enters the scene tree for the first time_left.
+#func _ready():
+#	var cube_node = cube_scene.instance()
+#	self.add_child(cube_node)
 	
-		if  count < 10:
-			randomize()
-			
-			var list = [ cube_scene.instance() , cylinder_scene.instance() , sphere_scene.instance() ]	
+
+# Called every frame. 'delta' is the elapsed time_left since the previous frame.
+func _process(delta):
+	# Make seed fo number.
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	# Make list of object's.
+	var list = [ cube_scene.instance() , cylinder_scene.instance() , sphere_scene.instance() ]
+
+
+	time_left += delta
+	if time_left > TIME_SPAN:
+		if  Node_counter < 10:
 			var drawned = list[randi() % list.size()]
-			var random_position = Vector3(randi() % 11 + 1 , 0, randi() % 11 + 1)
+			var random_position = Vector3(rng.randi_range(1,10) , 0, rng.randi_range(1,10) )
 			drawned.set_translation(random_position)
 			self.add_child(drawned)
-			count+=1
-		else:
-			pass
+			Node_counter += 1
+			# Reset time_left.
+			time_left = 0
+	else:
+		pass
