7b071fc2394dc3f089d64fd4491e0e334f4630cf
MarcinMirocha <60358010+Marcin-sys@users.noreply.github.com>
2020-03-06 00:42:56 +0100

    Fix typo & whitespace. Changed new object will deletes the earlier created object (the same type).

--- Gameculling/assets/code/spawner.gd
+++ Gameculling/assets/code/spawner.gd
@@ -4,34 +4,30 @@ var cube_scene = preload("res://scenes/cube.tscn")
 var cylinder_scene = preload("res://scenes/cylinder.tscn")
 var sphere_scene = preload("res://scenes/sphere.tscn")
 
-var Node_counter = 0	# For counting the number of objects on the screen
-const TIME_SPAN = 1     # Time period till function spawn another object
-var time_left = 0		# Timer
+var node_counter = 0					# For counting the number of objects on the screen
+const TIME_SPAN = 1    					# Time period till function spawn another object
+var time_left = 0						# Timer
+var rng = RandomNumberGenerator.new()  	# Make seed for number.
+var list								# Make list of object's.
+
 
 # Called when the node enters the scene tree for the first time_left.
-#func _ready():
-#	var cube_node = cube_scene.instance()
-#	self.add_child(cube_node)
-	
+func _ready():
+	rng.randomize()
+	list = [cube_scene.instance(), cylinder_scene.instance(), sphere_scene.instance()] 
 
 # Called every frame. 'delta' is the elapsed time_left since the previous frame.
 func _process(delta):
-	# Make seed fo number.
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	
-	# Make list of object's.
-	var list = [ cube_scene.instance() , cylinder_scene.instance() , sphere_scene.instance() ]
-
 
 	time_left += delta
+
 	if time_left > TIME_SPAN:
-		if  Node_counter < 10:
-			var drawned = list[randi() % list.size()]
-			var random_position = Vector3(rng.randi_range(1,10) , 0, rng.randi_range(1,10) )
-			drawned.set_translation(random_position)
-			self.add_child(drawned)
-			Node_counter += 1
+		if  node_counter < 10:
+			var temporary_object = list[randi() % list.size()]
+			var random_position = Vector3(rng.randi_range(1,10), 0, rng.randi_range(1,10))
+			temporary_object.set_translation(random_position)
+			self.add_child(temporary_object)
+			node_counter += 1
 			# Reset time_left.
 			time_left = 0
 	else:
