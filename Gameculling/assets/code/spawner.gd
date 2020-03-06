81f81ed3382e1794a93060b500750ecbbe3b758f
Qache <gortatm@gmail.com>
2020-03-06 21:10:32 +0100

    Code refactor. Fixed memory allocation bug.

--- Gameculling/assets/code/spawner.gd
+++ Gameculling/assets/code/spawner.gd
@@ -4,31 +4,27 @@ var cube_scene = preload("res://scenes/cube.tscn")
 var cylinder_scene = preload("res://scenes/cylinder.tscn")
 var sphere_scene = preload("res://scenes/sphere.tscn")
 
-var node_counter = 0					# For counting the number of objects on the screen
+var node_counter = 0					# For counting objects on the main scene
 const TIME_SPAN = 1    					# Time period till function spawn another object
 var time_left = 0						# Timer
-var rng = RandomNumberGenerator.new()  	# Make seed for number.
-var list								# Make list of object's.
-
+var rng  								# RNG object
+var scenes								# List of scenes
 
 # Called when the node enters the scene tree for the first time_left.
 func _ready():
+	rng = RandomNumberGenerator.new()
 	rng.randomize()
-	list = [cube_scene.instance(), cylinder_scene.instance(), sphere_scene.instance()] 
+	scenes = [cube_scene, cylinder_scene, sphere_scene] 
 
 # Called every frame. 'delta' is the elapsed time_left since the previous frame.
 func _process(delta):
-
-	time_left += delta
-
-	if time_left > TIME_SPAN:
-		if  node_counter < 10:
-			var temporary_object = list[randi() % list.size()]
-			var random_position = Vector3(rng.randi_range(1,10), 0, rng.randi_range(1,10))
-			temporary_object.set_translation(random_position)
-			self.add_child(temporary_object)
+	if node_counter < 10:
+		time_left += delta
+		if time_left > TIME_SPAN:
+			#Instancing new object on scene
+			var generated_object = scenes[randi() % scenes.size()].instance()
+			generated_object.set_translation(Vector3(rng.randi_range(1, 10), 0, rng.randi_range(1, 10)))
+			self.add_child(generated_object)
 			node_counter += 1
 			# Reset time_left.
 			time_left = 0
-	else:
-		pass
