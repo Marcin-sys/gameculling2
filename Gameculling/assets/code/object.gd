c3313da43d3be3cce257a34f8bba9ebc66f108d7
Qache <gortatm@gmail.com>
2020-03-26 19:40:03 +0100

    Despawning objects
    Objects are now despawning properly

--- Gameculling/assets/code/object.gd
+++ Gameculling/assets/code/object.gd
@@ -1,24 +1,20 @@
 extends Spatial
 
-
-var rng = RandomNumberGenerator.new() # Make seed for number.
 var my_object_id = 0
 var time_left = 0
-var time_when_disappear
+var time_to_death = 0
 
+func set_time_to_death(time: int):
+	time_to_death = time
 
 func _ready():
-	rng.randomize()
-	time_when_disappear = rng.randi_range(5, 10)
+	pass
 
 func _process(delta):
 
 
 	time_left += delta
-	print(time_when_disappear," : czas obiektu : ",time_left)
 	
-
-	if time_left > time_when_disappear:
+	if time_left > time_to_death:
 		print("jestem obiektem nr:", my_object_id , "i wsłaśnie umarłem")
 		queue_free()
-		time_left = 0 #TODO: usunac po dodaniu zabijania
