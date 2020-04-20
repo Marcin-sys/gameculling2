c3e534cfee8a81102f13c1ce8b5fc6fcb008c5da
MarcinMirocha <60358010+Marcin-sys@users.noreply.github.com>
2020-04-20 16:30:36 +0200

    WIP multimesh
    Added Multimesh

--- Gameculling/assets/code/MultiMeshInstance.gd
+++ Gameculling/assets/code/MultiMeshInstance.gd
@@ -1,18 +1,20 @@
 extends MultiMeshInstance
-
-
+var new_position = 0
+var change_position = 10 # position where to spawn another mesh 
 func _ready():
-    # Create multimesh resource that will contain 4 cubes
-    var mm = MultiMesh.new()
-    mm.transform_format = MultiMesh.TRANSFORM_3D
-    mm.instance_count = 4
-    mm.mesh = preload("res://cube.obj")
+	# Create multimesh resource that will contain 4  mesh
+	var mm = MultiMesh.new()
+	mm.transform_format = MultiMesh.TRANSFORM_3D
 
-    # Set position of all cubes at random
-    for i in range(mm.instance_count):
-        var pos = Vector3(rand_range(-2, 2), rand_range(-2, 2), rand_range(-2, 2))
-        var t = Transform(Basis(), pos)
-        mm.set_instance_transform(i, t)
+	mm.instance_count = 4
+	
+	#mm.mesh = preload("res://scenes/Mesh_scene.tscn")
 
-    # Assign multimesh to be rendered by the MultiMeshInstance
-    self.multimesh = mm
+	# Set mesh
+	for i in range(mm.instance_count):
+		var pos = Vector3(new_position,0,0)
+		new_position += change_position
+		mm.set_instance_transform(i, Transform(Basis(), pos))
+
+	# Assign multimesh to be rendered by the MultiMeshInstance
+	self.multimesh = mm
--- Gameculling/assets/code/generate_mesh.gd
+++ Gameculling/assets/code/generate_mesh.gd
@@ -6,35 +6,15 @@ var UVs = PoolVector2Array()          #lista Vector2
 var mat = SpatialMaterial.new()
 var position_x_x
 func _ready():
-	for i in range(0,110,10):
+	for i in range(0,10,10):   # if y = 110 => 10 x 10 
 		var position_x = 0 + i
-		print(i)
+		
 		for i in range(0,110,10):
 			var position_z = 0 + i
 			for i in range(0,20,10):
 				position_x_x = i
 				vertices.push_back(Vector3(position_x_x + position_x, 0, 0 + position_z))
-
-		# vertices.push_back(Vector3(0+position_x,0,10))  #first
-		# vertices.push_back(Vector3(10+position_x,0,10))
-		# vertices.push_back(Vector3(0+position_x,0,20))
-		# vertices.push_back(Vector3(10+position_x,0,20))
-		# vertices.push_back(Vector3(0+position_x,0,30)) #five
-		# vertices.push_back(Vector3(10+position_x,0,30))
-		# vertices.push_back(Vector3(0+position_x,0,40)) #five
-		# vertices.push_back(Vector3(10+position_x,0,40))
-		# vertices.push_back(Vector3(0+position_x,0,50)) #five
-		# vertices.push_back(Vector3(10+position_x,0,50))
-		# vertices.push_back(Vector3(0+position_x,0,60)) #five
-		# vertices.push_back(Vector3(10+position_x,0,60))
-		# vertices.push_back(Vector3(0+position_x,0,70)) #five
-		# vertices.push_back(Vector3(10+position_x,0,70))
-		# vertices.push_back(Vector3(0+position_x,0,80)) #five
-		# vertices.push_back(Vector3(10+position_x,0,80))
-		# vertices.push_back(Vector3(0+position_x,0,90)) #five
-		# vertices.push_back(Vector3(10+position_x,0,90))
-		# vertices.push_back(Vector3(0+position_x,0,100)) #five
-		# vertices.push_back(Vector3(10+position_x,0,100))
+	
 
 		# UVs.push_back(Vector2(0,0))
 		# UVs.push_back(Vector2(0.5,0))
